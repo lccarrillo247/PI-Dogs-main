d3fc5167fd0c5d7aa5f7bba7debcd98d064437f9
lccarrillo247 <lccarrillo247@gmail.com>
2023-09-26 10:21:53 -0500

    Carga inicial PI

---
+++ api/src/models/Dogs.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Dogs', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    añosDeVida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+};
